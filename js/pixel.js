d81cd14eb845feaaae491bee86afa48ffd32f8f9
Philip Mulyana <philip@philipmulyana.com>
2026-03-03 17:05:16 +0700

    Add Facebook Pixel (1408957391027533) to all pages

---
+++ js/pixel.js
@@ -0,0 +1,26 @@
+// Facebook Pixel - Philip Mulyana Website
+// Pixel ID: 1408957391027533
+// Include this script in every page: <script src="js/pixel.js"></script>
+
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1408957391027533');
+fbq('track', 'PageView');
+
+// noscript fallback
+(function() {
+    var noscript = document.createElement('noscript');
+    var img = document.createElement('img');
+    img.height = 1;
+    img.width = 1;
+    img.style.display = 'none';
+    img.src = 'https://www.facebook.com/tr?id=1408957391027533&ev=PageView&noscript=1';
+    noscript.appendChild(img);
+    document.body.appendChild(noscript);
+})();
